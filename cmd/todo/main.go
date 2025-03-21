cc8370463b10f4a7be5f0330f14c352355f9afab
GabrielSkogHD <skoog.gabriel02@gmail.com>
2025-03-21 23:52:27 +0100

    del, add , complete and list

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	todo "my-go-project"
+	"os"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark as complete")
+	del := flag.Int("del", 0, "deleta a todo")
+	list := flag.Bool("list", false, "list all todos")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
 	"time"
 )
 
@@ -13,6 +16,14 @@ type item struct {
 }
 
 type Todos []item
+
+func (t *Todos) Print() {
+
+	for i, item := range *t {
+		fmt.Printf("%d - %s\n", i, item.Task)
+	}
+
+}
 
 func (t *Todos) Add(task string) {
 
@@ -50,3 +61,34 @@ func (t *Todos) Delete(index int) error {
 }
 
 //https://www.youtube.com/watch?v=j1CXoOQXbco 10:03
+
+func (t *Todos) Load(filename string) error {
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+
+	}
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (t *Todos) Store(filename string) error {
+
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, 0644)
+}
